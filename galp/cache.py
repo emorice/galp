41b0e73bf6c0d06a835bc40e63b78840ac5a4597
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2022-03-29 19:16:46 +0200

    cache: removing native cache, closes #25
    This removes any in-memory caching on fs-backed workers.

--- galp/cache.py
+++ galp/cache.py
@@ -17,9 +17,6 @@ class CacheStack():
     def __init__(self, dirpath, serializer):
         self.dirpath = dirpath
 
-        self.nativecache = dict()
-        self._handles = dict()
-
         if dirpath is None:
             self.serialcache = dict()
         else:
@@ -29,43 +26,29 @@ class CacheStack():
 
 
     def contains(self, name):
-        # Mind the short circuit
-        return (name in self.nativecache) or (name + b'.proto' in self.serialcache)
+        return name + b'.proto' in self.serialcache
 
     def get_native(self, handle):
         """
         Get a native object form the cache.
 
         Either finds it in memory, or deserialize from persistent storage.
-        As a small optimization, in the latter case the native object is also
-        stored in memory.
         """
-        try:
-            # Direct from memory
-            return self.nativecache[handle.name]
-        except KeyError:
-            # Deserialize from cold storage
-            proto, data, b_children = self.get_serial(handle.name)
-            children = int.from_bytes(b_children, 'big')
-            native_children = [
-                self.get_native(
-                    handle[i]
-                    )
-                for i in range(children)
-                ]
+        proto, data, b_children = self.get_serial(handle.name)
+        children = int.from_bytes(b_children, 'big')
+        native_children = [
+            self.get_native(
+                handle[i]
+                )
+            for i in range(children)
+            ]
 
-            native = self.serializer.loads(handle, proto, data, native_children)
-            # Hook into memory cache
-            self.nativecache[handle.name] = native
-            self._handles[handle.name] = handle
-            return native
+        native = self.serializer.loads(handle, proto, data, native_children)
+        return native
 
     def get_serial(self, name):
         """
         Get a serialized object form the cache.
-
-        For now we prioritize serializing each time from memory, but this could
-        change.
 
         Returns:
             a triplet of bytes objects (proto, data, children). Note that
@@ -73,32 +56,25 @@ class CacheStack():
             If there is no data, None is returned instead.
         """
         try:
-            # Serialize from memory
-            native = self.nativecache[name]
-            handle = self._handles[name]
-            return self.serializer.dumps(handle, native) + (int(0).to_bytes(1, 'big'),)
+            data = self.serialcache[name + b'.data']
         except KeyError:
-            # Direct from persistent storage
-            try:
-                data = self.serialcache[name + b'.data']
-            except KeyError:
-                data = None
-            try:
-                children = self.serialcache[name + b'.children']
-            except KeyError:
-                children = int(0).to_bytes(1, 'big')
-            proto = self.serialcache[name + b'.proto']
-            return (
-                proto,
-                data,
-                children
-                )
+            data = None
+        try:
+            children = self.serialcache[name + b'.children']
+        except KeyError:
+            children = int(0).to_bytes(1, 'big')
+        proto = self.serialcache[name + b'.proto']
+        return (
+            proto,
+            data,
+            children
+            )
                 
 
     def put_native(self, handle, obj):
         """Puts a native object in the cache.
 
-        For now this also eagerly serialize it and commit it to persistent cache.
+        For now this eagerly serializes it and commits it to persistent cache.
         Recursive call if handle is iterable.
         """
         try:
@@ -108,11 +84,7 @@ class CacheStack():
                 self.put_native(sub_handle, sub_obj)
 
             self.put_serial(handle.name, b'tuple', None, len(obj).to_bytes(1, 'big'))
-
         except NonIterableHandleError:
-            self.nativecache[handle.name] = obj
-            self._handles[handle.name] = handle
-
             proto, data = self.serializer.dumps(handle, obj)
             self.put_serial(handle.name, proto, data, int(0).to_bytes(1, 'big'))
 
