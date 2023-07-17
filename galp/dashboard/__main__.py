b8e10360215faad157b4a9032fc5a3524e1de833
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2023-07-17 16:07:41 +0200

    feat(dashboard): make http serving port changeable

--- galp/dashboard/__main__.py
+++ galp/dashboard/__main__.py
@@ -9,14 +9,22 @@ import galp.cli
 
 from . import create_app
 
+def add_parser_arguments(_parser):
+    """
+    Add dashboard-specific options to server
+    """
+    _parser.add_argument('-p', '--port', help='Dashboard HTTP port', type=int,
+            default=5000)
+
 parser = argparse.ArgumentParser()
 galp.cache.add_store_argument(parser, optional=True)
 galp.cli.add_parser_arguments(parser)
+add_parser_arguments(parser)
 args = parser.parse_args()
 galp.cli.setup('dashboard', args.log_level)
 
 app = create_app(vars(args))
 
-with make_server('', 5000, app) as httpd:
-    print("Serving HTTP on port 5000", flush=True)
+with make_server('', args.port, app) as httpd:
+    print("Serving HTTP on port", args.port, flush=True)
     httpd.serve_forever()
