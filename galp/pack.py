5d6068b838e693ffd7cd3be22123a504273cf9a5
Étienne Morice <pro.emorice@mail.com>
2024-08-17 22:13:13 +0200

    refactor(galp): use a dict of loader functions
    Then all of the loading type introspection can be done up-front and cached,
    which is marginally faster, but most importantly this should make it easy to
    plug custom loaders.

--- galp/pack.py
+++ galp/pack.py
@@ -9,7 +9,7 @@ import logging
 from dataclasses import fields, is_dataclass
 from functools import singledispatch
 from typing import (TypeVar, get_args, get_origin, Any, Annotated, Literal,
-                    Union, Generic)
+                    Union, Generic, Callable, TypeAlias)
 from collections.abc import Hashable
 from types import NoneType, UnionType
 
@@ -36,6 +36,7 @@ class TypeMap(Generic[T]):
     """
     def __init__(self, types: dict[Hashable, type[T]]):
         self.from_key = {}
+        self.loaders = {}
         @singledispatch
         def _get_key(obj: T) -> Hashable:
             raise LoadException(obj)
@@ -43,6 +44,7 @@ class TypeMap(Generic[T]):
         for key, cls in types.items():
             self.from_key[key] = cls
             self.get_key.register(cls, lambda _obj, key=key: key)
+            self.loaders[key] = get_loader(cls)
 
     def dump_part(self, obj: T) -> tuple[object, list[bytes]]:
         """Dump an object with the union member key"""
@@ -58,7 +60,7 @@ class TypeMap(Generic[T]):
                   ) -> tuple[T, list[bytes]]:
         """Load a union member according to key"""
         key, value_doc = doc
-        return load_part(self.from_key[key], value_doc, stream)
+        return self.loaders[key](value_doc, stream)
 
     def load(self, msg: list[bytes]) -> Ok[T] | LoadError:
         """Load union member from finalized root doc"""
@@ -80,33 +82,98 @@ class TypeMap(Generic[T]):
             raise TypeError
         return cls(dict(enumerate(get_args(union))))
 
+Loader: TypeAlias = Callable[[type[T], list[bytes]], tuple[T, list[bytes]]]
+
+_LOADERS: dict[type, Loader] = {}
+"""
+Cache the loading function for types already seen
+"""
+
+def get_loader(cls: type[T]) -> Loader[T]:
+    """Get loader for cls, creating and caching it if necessary"""
+    if cls in _LOADERS:
+        return _LOADERS[cls]
+    if is_dataclass(cls):
+        loader = make_load_dataclass(cls)
+    else:
+        loader = make_load_builtin(cls)
+    _LOADERS[cls] = loader
+    return loader
+
 L = TypeVar('L', list, tuple)
-def load_list(cls: type[L], doc: object, stream: list[bytes]
-             ) -> tuple[L, list[bytes]]:
+def make_load_list(cls: type[L]) -> Loader[L]:
     """Load a list or tuple"""
-    if not isinstance(doc, list):
-        raise LoadException('list expected')
-    obj = []
+
     type_var, *_ellipsis = get_args(cls)
     assert _ellipsis in ([], [...]), 'not supported yet'
-    for item_doc in doc:
-        item, stream = load_part(type_var, item_doc, stream)
-        obj.append(item)
-    return cls(obj), stream
+    load_item = get_loader(type_var)
+
+    def _load_list(doc: object, stream: list[bytes]
+                 ) -> tuple[L, list[bytes]]:
+        if not isinstance(doc, list):
+            raise LoadException('list expected')
+        obj = []
+        for item_doc in doc:
+            item, stream = load_item(item_doc, stream)
+            obj.append(item)
+        return cls(obj), stream
+    return _load_list
 
 D = TypeVar('D', bound=dict)
-def load_dict(cls: type[D], doc: object, stream: list[bytes]
-             ) -> tuple[D, list[bytes]]:
+def make_load_dict(cls: type[D]) -> Loader[D]:
     """Load a dict"""
-    if not isinstance(doc, dict):
-        raise LoadException('dict expected')
-    obj = cls()
     key_type_var, value_type_var = get_args(cls)
-    for key_item_doc, value_item_doc in doc.items():
-        key_item, stream = load_part(key_type_var, key_item_doc, stream)
-        value_item, stream = load_part(value_type_var, value_item_doc, stream)
-        obj[key_item] = value_item
-    return obj, stream
+    load_key = get_loader(key_type_var)
+    load_value = get_loader(value_type_var)
+    def _load_dict(doc: object, stream: list[bytes]
+                 ) -> tuple[D, list[bytes]]:
+        if not isinstance(doc, dict):
+            raise LoadException('dict expected')
+        obj = cls()
+        for key_item_doc, value_item_doc in doc.items():
+            key_item, stream = load_key(key_item_doc, stream)
+            value_item, stream = load_value(value_item_doc, stream)
+            obj[key_item] = value_item
+        return obj, stream
+    return _load_dict
+
+def make_load_literal(cls: type[T]) -> Loader[T]:
+    """Load a literal (constant)"""
+    value, = get_args(cls)
+    def _load(doc: object, stream: list[bytes]) -> tuple[T, list[bytes]]:
+        if doc != value:
+            raise LoadException(f'{value} expected')
+        return value, stream
+    return _load
+
+def load_none(doc: object, stream: list[bytes]) -> tuple[None, list[bytes]]:
+    """Load None"""
+    if doc is not None:
+        raise LoadException('None expected')
+    return None, stream
+
+def make_load_default(cls: type[T]) -> Loader[T]:
+    """Load other basic types: int, float, bool, str, bytes"""
+    def _load(doc: object, stream: list[bytes]) -> tuple[T, list[bytes]]:
+        # For all remaining basic types, we assume they can be coerced by
+        # calling the constructor
+        return cls(doc), stream
+    return _load
+
+def make_load_builtin(cls: type[T]) -> Loader[T]:
+    """
+    Load basic (non-dataclass) types
+    """
+    orig = get_origin(cls)
+    if orig in (list, tuple):
+        return make_load_list(cls)
+    if orig is dict:
+        return make_load_dict(cls)
+    if orig is Literal:
+        return make_load_literal(cls)
+    if cls is NoneType:
+        return load_none
+    return make_load_default(cls)
 
 def check_payload(cls):
     """Parse and strip paylaod and other annotations"""
@@ -115,7 +182,7 @@ def check_payload(cls):
         return orig_cls, _IsPayload in annotations
     return cls, False
 
-_FIELDS_CACHE: dict[object, list[tuple[str, bool, TypeMap | None, Any]]] = {}
+_FIELDS_CACHE: dict[object, list[tuple[str, bool, TypeMap | None, Any, Loader]]] = {}
 
 def parse_fields(cls):
     """
@@ -130,38 +197,49 @@ def parse_fields(cls):
         except TypeError:
             typemap = None
 
+        if typemap:
+            loader = typemap.load_part
+        else:
+            loader = get_loader(orig_cls)
+
         field_list.append((
             f.name,
             is_payload,
             typemap,
-            orig_cls))
+            orig_cls,
+            loader))
 
     _FIELDS_CACHE[cls] = field_list
     return field_list
 
-def load_builtin(cls: type[T], doc: object, stream: list[bytes]
-              ) -> tuple[T, list[bytes]]:
+def make_load_dataclass(cls: type[T]) -> Loader[T]:
     """
-    Load basic (non-dataclass) types
+    Generate a load function for a specific dataclass
     """
-    orig = get_origin(cls)
-    if orig in (list, tuple):
-        # mypy doesn't like this way of narrowing cls
-        return load_list(cls, doc, stream) # type: ignore[type-var]
-    if orig is dict:
-        return load_dict(cls, doc, stream) # type: ignore[type-var]
-    if orig is Literal:
-        value, = get_args(cls)
-        if doc != value:
-            raise LoadException(f'{value} expected')
-        return value, stream # type: ignore[call-arg]
-    if cls is NoneType:
-        if doc is not None:
-            raise LoadException('None expected')
-        return None, stream # type: ignore[return-value]
-    # For all remaining basic types, we assume they can be coerced by calling
-    # the constructor: int, float, bool, str, bytes
-    return cls(doc), stream # type: ignore[call-arg]
+    field_list = parse_fields(cls)
+
+    def _load(doc: object, stream: list[bytes]) -> tuple[T, list[bytes]]:
+        if not isinstance(doc, dict):
+            raise LoadException
+        obj_dict = dict(doc)
+        for name, is_payload, _typemap, item_cls, loader in field_list:
+            if is_payload:
+                # Get data for this attribute from the rest of the stream
+                attr_doc_buf, *stream = stream
+                # Deserialize it, but shortcut bytes object
+                if item_cls is bytes:
+                    attr_doc: Any = attr_doc_buf
+                else:
+                    attr_doc = msgpack.loads(attr_doc_buf)
+            else:
+                # Get data from the main dict
+                attr_doc = obj_dict[name]
+
+            # Then apply the corresponding loader
+            # The loader can consume further stream items
+            obj_dict[name], stream = loader(attr_doc, stream)
+        return cls(**obj_dict), stream # type: ignore[return-value] # what
+    return _load
 
 def load_part(cls: type[T], doc: object, stream: list[bytes]
               ) -> tuple[T, list[bytes]]:
@@ -169,38 +247,12 @@ def load_part(cls: type[T], doc: object, stream: list[bytes]
     Comsume an object of type `cls` from the docstream.
     Depending on number of payloads, there may be leftover items that are returned.
     """
-    # Trivial guard for basic types
-    if not is_dataclass(cls):
-        return load_builtin(cls, doc, stream)
-
-    if not isinstance(doc, dict):
-        raise LoadException
-    obj_dict = dict(doc)
     try:
-        field_list = _FIELDS_CACHE[cls]
+        return _LOADERS[cls](doc, stream)
     except KeyError:
-        field_list = parse_fields(cls)
+        pass
 
-    for name, is_payload, typemap, item_cls in field_list:
-        if is_payload:
-            # Get data for this attribute from the rest of the stream
-            attr_doc_buf, *stream = stream
-            # Deserialize it, but shortcut bytes object
-            if item_cls is bytes:
-                attr_doc: Any = attr_doc_buf
-            else:
-                attr_doc = msgpack.loads(attr_doc_buf)
-        else:
-            # Get data from the main dict
-            attr_doc = obj_dict[name]
-
-        # Then apply the corresponding loader
-        # The loader can consume further stream items
-        if typemap:
-            obj_dict[name], stream = typemap.load_part(attr_doc, stream)
-        else:
-            obj_dict[name], stream = load_part(item_cls, attr_doc, stream)
-    return cls(**obj_dict), stream # type: ignore[return-value] # what
+    return get_loader(cls)(doc, stream)
 
 def dump_list(obj: list | tuple) -> tuple[list, list[bytes]]:
     """Dump a list or tuple"""
@@ -246,7 +298,7 @@ def dump_part(obj: object) -> tuple[object, list[bytes]]:
     except KeyError:
         field_list = parse_fields(type(obj))
 
-    for name, is_payload, typemap, item_cls in field_list:
+    for name, is_payload, typemap, item_cls, _loader in field_list:
         # Apply the dumper for this attribute
         # The dumper can produce further stream items
         if typemap:
