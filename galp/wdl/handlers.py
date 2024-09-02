ca9d8ff44238e9aa9cd6625f738d06fa5f8bd56a
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2024-09-02 18:38:40 +0200

    fix(wdl): better error message on missing miniwdl

--- galp/wdl/handlers.py
+++ galp/wdl/handlers.py
@@ -4,9 +4,16 @@ Globally nuke wdl signal handlers
 In the long run we will absorb this logic
 """
 
+import logging
 from contextlib import contextmanager
 
-import WDL._util
+try:
+    import WDL._util
+except ModuleNotFoundError as exc:
+    logging.error('The galp.wdl extension requires the wdl galp extra'
+            ' dependencies, which can be for instance installed with:\n\n'
+            '    pip install galp[wdl]\n')
+    raise
 
 @contextmanager
 def _noop(*_, **_k):
